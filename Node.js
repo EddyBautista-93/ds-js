309eba04c363e30db48f2927da03b13825a9d9e8
EddyBautista <bautista.j.eddy@gmail.com>
2021-11-23 04:02:06 -0600

    setting up node class for lec

--- Node.js
+++ Node.js
@@ -1,11 +1,19 @@
-class Node{
+class Node {
     constructor(data) {
-        this.value = data;
+      this.data = data;
+      this.next = null;
     }
-}
-
-const firstNode = new Node(3);
-
-console.log(firstNode);
-
-module.export = Node;
+  
+    setNextNode(node) {
+      this.next = node;
+    }
+  }
+  
+  const firstNode = new Node('I am an instance of a Node!');
+  const secondNode = new Node('I am the next Node!');
+  
+  firstNode.setNextNode(secondNode);
+  console.log(firstNode);
+  
+  module.exports = Node;
+  
